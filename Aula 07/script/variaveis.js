cd1d0b7ca453d48b775b7b1ece7f4e1d6a6cf3ce
Brunno Silva <124358824+BrunnoSA@users.noreply.github.com>
2023-02-07 11:29:07 -0300

    Add files via upload

---
+++ Aula 07/script/variaveis.js
@@ -0,0 +1,13 @@
+//tem duas formas de declarar uma variavel
+// var e let
+/*let hello = 'olá turma da manhã'
+let mensagem;
+
+mensagem = hello;
+
+alert(hello)
+alert(mensagem);*/
+
+const carro = ["fiat", "Golf", "BMW", "Ferrari"];
+
+alert(carro);
